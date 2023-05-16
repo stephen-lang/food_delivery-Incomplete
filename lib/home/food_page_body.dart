7dc4da83a5e40d3f189628e75a91eb92e8b9fde6
Stephen <stephenfianu@gmail.com>
2023-05-16 05:18:39 +0000

    '4'

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:food_delivery/Widgets/BigText.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -24,20 +26,55 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   Widget _buildPageItem(int index) {
     return Stack(children: [
       Container(
-        alignment: AlignmentDirectional.bottomEnd,
         height: 180,
-        margin: const EdgeInsets.only(left: 20, right: 20),
+        margin: const EdgeInsets.only(
+          left: 10,
+          right: 10,
+        ),
         decoration: BoxDecoration(
-          color: index.isEven ? Colors.brown : Colors.red,
+          color: index.isEven ? Colors.red : Colors.red,
+          image: const DecorationImage(
+              fit: BoxFit.fill, image: AssetImage("assets/image/food1.jpg")),
           borderRadius: BorderRadius.circular(30),
         ),
       ),
       Container(
-        height: 180,
-        margin: const EdgeInsets.only(left: 40, right: 40),
+        alignment: AlignmentDirectional.bottomEnd,
+        height: 140,
+        width: 300,
+        margin: const EdgeInsets.only(left: 60, right: 40, top: 140),
         decoration: BoxDecoration(
-          color: index.isEven ? Colors.brown : Colors.blue,
+          color: index.isEven ? Colors.red : Colors.blue,
           borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          children: [
+            BigText(
+                text: "Bitter Orange Marinade",
+                size: 50,
+                textOverflow: TextOverflow.visible),
+            Row(
+              children: [
+                RatingBar.builder(
+                  initialRating: 3,
+                  itemSize: 15,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                )
+              ],
+            ),
+            Row(),
+          ],
         ),
       ),
     ]);
